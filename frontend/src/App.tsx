811ea3ceee740111e5491581b427948f67fb31f4
Denes Alessandro Reis <denesreis@scasistemas.com.br>
2021-11-06 20:22:50 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,10 @@
-import BarChart from "components/barChart";
-import DataTable from "components/dataTable";
-import DonutChart from "components/donutChart";
-import Footer from "components/footer";
-import NavBar from "components/navBar";
+import Routes from "Routes";
 
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container"> 
-        <h1 className="text-primary py-3">DashBoard de Vendas</h1> 
-        <div className="row px-3"> 
-          <div className="col-sm-6"> 
-            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5> 
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas a Vendas</h5>
-            <DonutChart />
-          </div> 
+    <Routes />
 
-        </div>
-        <div className="py-3">
-          <h2 className="text-primary"> Todas as Vendas</h2>
-
-        </div>
-
-
-
-        <DataTable />
-      </div>
-      <Footer />
-    </>
-        );
+  );
 }
-
-        export default App;
+export default App;
