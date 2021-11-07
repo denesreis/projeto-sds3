bef07b5a64502e35a2e0f8c9c7dd571b022ee14b
Denes Alessandro Reis <denesreis@scasistemas.com.br>
2021-11-07 08:40:28 -0300

    First request

--- frontend/src/components/donutChart/index.tsx
+++ frontend/src/components/donutChart/index.tsx
@@ -1,11 +1,35 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+//Não usa o export porque vai ser usado somente neste modulo
+type ChartData = {
+    labels : string[];
+    series : number[];
+
+}
 
 const DonutChart = () => {
+    //Importante  é let e não const porque vai mudar o valor 
+    let chartData : ChartData = { labels:[] , series:[] }; //Instanciando do conteudo mais simples possível
 
-    const mockData = {
+    //Usando a crase digita tudo e as variáveis ficam entre chaves
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        //o código é equivalente a (response) => {console.log(response.data)} response é parametro da função then
+        //como pe apenas um parametro não precisa do abre e fecha parenteses
+        .then(response => {
+            const data = response.data as SaleSum[]; //fazendo o cast convertendo a resposta em SaleSum definido em types/sale
+            const myLabels = data.map(x => x.saleName)
+            const mySeries = data.map(x => x.sum)
+            chartData = {labels : myLabels, series : mySeries} //Atribuindo os valores para o chartData
+            console.log(chartData)
+        });
+
+
+   /* const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    }*/
     
     const options = {
         legend: {
@@ -16,8 +40,8 @@ const DonutChart = () => {
     return (
 
         <Chart //passando os parametos para o componente Chart
-            options = {{...options, labels: mockData.labels}} //...options indica para pegar o componete options como ele esta e depois acrescentar mais outras coisas/ xaxis = eixo X
-            series = {mockData.series}
+            options = {{...options, labels: chartData.labels}} //...options indica para pegar o componete options como ele esta e depois acrescentar mais outras coisas/ xaxis = eixo X
+            series = {chartData.series}
             type = "donut"
             height = "240"
         
